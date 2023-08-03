b761960c9545d0331c5bd28f737feaba85b2664d
themuuln <zerone.offical@gmail.com>
2023-08-03 18:07:36 +0800

    script updated

--- install_tools.ps1
+++ install_tools.ps1
@@ -1,24 +1,24 @@
-
 # Check if running as administrator, if not, restart script as administrator
-if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
-{
-  Write-Host "This script requires administrator privileges. Restarting with elevated permissions..."
-  Start-Sleep -Seconds 3
-  Start-Process powershell.exe -Verb RunAs -ArgumentList ($MyInvocation.MyCommand.Definition)
-  Exit
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Host "This script requires administrator privileges. Restarting with elevated permissions..."
+    Start-Sleep -Seconds 3
+    Start-Process powershell.exe -Verb RunAs -ArgumentList ($MyInvocation.MyCommand.Definition)
+    Exit
 }
 
 # Install Scoop if not already installed
-if (-not (Get-Command scoop -ErrorAction SilentlyContinue))
-{
-  Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
+    Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
 }
 
 # Install Neovim, Node.js, Python, Git, PSReadLine, Fzf, Yarn, Z via Scoop
 $packages = @('neovim', 'nodejs', 'python', 'git', 'psreadline', 'fzf', 'yarn', 'z')
 $packages | ForEach-Object {
-  scoop install $_
+    scoop install $_
 }
+
+# Install z module
+Install-Module -Name z -Force
 
 # Add scoop's bin directory to the system PATH and reload the environment variables
 $binPath = Join-Path $env:USERPROFILE 'scoop\shims'
